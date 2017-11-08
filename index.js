4f11bab84bc180225b6c5877bdca6eccd2094931
icnpdesign <inp@bu.edu>
2017-11-08 16:30:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,8 @@ function ternaryCheckCity (city) {
 function switchOnCharmFromTip(tip) {
   if (tip == 'generous') {
     return 'Thank you so much.';
-  } else if (tip == 'not as generous') { 
-    return 'Thank you.'; 
+  } else if (tip == 'not as generous') {
+    return 'Thank you.';
   } else { return 'Bye.';
   }
 }
-
