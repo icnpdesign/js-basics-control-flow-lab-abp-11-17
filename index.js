cda52e946c460862b0f929e7026abfb6b11caf6b
icnpdesign <inp@bu.edu>
2017-11-08 16:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ function scuberGreetingForFeet (distance) {
 } else { return 'No can do.'}
 }
 
-destination = NYC ? return 'Ok, sounds good.' : return 'No go.'
+function ternaryCheckCity (destination) {
+  destination = NYC ? return 'Ok, sounds good.' : return 'No go.'
+}
